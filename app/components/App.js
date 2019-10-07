c16a3a6f37c67c8b712a68b09cb48c7737942308
vidul <vidul.verma@outlook.com>
2019-10-07 10:19:50 -0700

    fixes

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import 'es6-symbol/implement';
+// import 'es6-symbol/implement';
 
 import Nav from '../config/routes';
 
@@ -8,22 +8,31 @@ import Nav from '../config/routes';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux'
 import combiner from '../reducers/Combiner'
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
 
+import { View, Text } from 'react-native'
 export const store = createStore(
-    combiner,
-    applyMiddleware(thunk, logger)
+    combiner
 );
 
+//provider makes sure that store is available throughout the app
+// <Provider store={store}>
+//     <Nav
+//         ref={
+//             nav => {
+//                 this.navigator = nav;
+//             }
+//         } />
+// </Provider>
 
 export default class App extends React.Component {
 
     render() {
-        //DEBUG ONLY
-        console.disableYellowBox = true;
         return (
-            //provider makes sure that store is available throughout the app
+            // <View>
+            //     <Text>Hello world</Text>
+            // </View>
             <Provider store={store}>
                 <Nav
                     ref={
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './app/components/App';
-import 'es6-symbol/implement';
 
 
 // window.LOG_LEVEL='DEBUG';
