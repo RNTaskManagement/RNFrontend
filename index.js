574d3afb3852ab5fbd8780ee27b45801f8f2b1af
vidul <vidul.verma@outlook.com>
2019-10-11 20:11:54 -0700

    package fixes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './app/components/App';
 
 
-AppRegistry.registerComponent('TeamTasks', () => App);
+AppRegistry.registerComponent('teamtasks', () => App);
 
